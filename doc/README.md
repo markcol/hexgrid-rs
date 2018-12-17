d9f8e9cd0325dbd8a6270e0dc4dcaa5cf487d249
Mark H. Colburn <colburn.mark@gmail.com>
2018-12-17 09:01:03 -0800

    Add basic hex functions.

---
+++ doc/README.md
@@ -0,0 +1,9 @@
+# hex
+
+# Notes
+
+Both Star Fleet Battles and Wooden Ships & Iron Men use a flat-topped hexagon map with the origin at the top left, using odd-offset numbering.
+
+Star Fleet Battles uses a coordinate naming system, where each hex is numbered based on a two digit representation of x and y position concatenated into a single 4-digit coordinate name. (0101, 0102, etc.)
+
+Wooden Ships and Iron Men uses a letter-digit representation, where the X coordinate is represented as a Letter, and the Y coordinate is represented as a number. (A1, B1, ... AA1, etc.)
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! A library to work with hexgrids written in Rust.
+//! A library to work with hex grids written in Rust.
 
 #![forbid(overflowing_literals)]
 #![warn(
@@ -28,12 +28,184 @@
     clippy::too_many_arguments
 )]
 
+use std::ops::{Add, AddAssign};
+use std::ops::{Sub, SubAssign};
+use std::ops::{Mul, MulAssign};
+use std::ops::{Div, DivAssign};
+
+#[derive(Debug, PartialEq)]
+pub struct Hex {
+    pub q: i32,
+    pub r: i32,
+    pub s: i32,
+}
+
+impl Hex {
+    pub fn new(q: i32, r:i32, s:i32) -> Self {
+        Hex {
+            q,
+            r,
+            s,
+        }
+    }
+
+    pub fn distance(self, other: Hex) -> usize {
+        let l = self - other;
+        ((l.q.abs() + l.r.abs() + l.s.abs()) / 2) as usize
+    }
+}
+
+impl Add for Hex {
+    type Output = Hex;
+
+    fn add(self, other: Hex) -> Hex {
+        Hex {
+            q: self.q + other.q,
+            r: self.r + other.r,
+            s: self.s + other.s,
+        }
+    }
+}
+
+impl AddAssign for Hex {
+    fn add_assign(&mut self, other: Hex) {
+        self.q += other.q;
+        self.r += other.r;
+        self.s += other.s;
+    }
+}
+
+impl Sub for Hex {
+    type Output = Hex;
+
+    fn sub(self, other: Hex) -> Hex {
+        Hex {
+            q: self.q - other.q,
+            r: self.r - other.r,
+            s: self.s - other.s,
+        }
+    }
+}
+
+impl SubAssign for Hex {
+    fn sub_assign(&mut self, other: Hex) {
+        self.q -= other.q;
+        self.r -= other.r;
+        self.s -= other.s;
+    }
+}
+
+
+impl Div for Hex {
+    type Output = Hex;
+
+    fn div(self, other: Hex) -> Hex {
+        Hex {
+            q: self.q / other.q,
+            r: self.r / other.r,
+            s: self.s / other.s,
+        }
+    }
+}
+
+impl DivAssign for Hex {
+    fn div_assign(&mut self, other: Hex) {
+        self.q /= other.q;
+        self.r /= other.r;
+        self.s /= other.s;
+    }
+}
+
+
+impl Mul for Hex {
+    type Output = Hex;
+
+    fn mul(self, other: Hex) -> Hex {
+        Hex {
+            q: self.q * other.q,
+            r: self.r * other.r,
+            s: self.s * other.s,
+        }
+    }
+}
+
+impl MulAssign for Hex {
+    fn mul_assign(&mut self, other: Hex) {
+        self.q *= other.q;
+        self.r *= other.r;
+        self.s *= other.s;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_it() {
-        assert!(true);
+    fn equality() {
+        let a = Hex::new(1, 2, 3);
+        let b = Hex::new(1, 2, 3);
+        assert_eq!(&a, &b);
+    }
+
+    #[test]
+    fn inequality() {
+        let a = Hex::new(1, 2, 3);
+        let b = Hex::new(2, 2, 3);
+        assert_ne!(&a, &b);
+    }
+
+    #[test]
+    fn add() {
+        assert_eq!(Hex{q:1,r:2,s:3} + Hex{q:4,r:5,s:6}, Hex{q:5,r:7,s:9});
+    }
+
+    #[test]
+    fn add_assign() {
+        let mut a = Hex::new(1, 2, 3);
+        a += Hex{q: 4, r:5, s:6};
+        assert_eq!(a, Hex{q:5,r:7,s:9});
+    }
+
+    #[test]
+    fn sub() {
+        assert_eq!(Hex{q:5,r:7,s:9} - Hex{q:4,r:5,s:6}, Hex{q:1,r:2,s:3});
+    }
+
+    #[test]
+    fn sub_assign() {
+        let mut a = Hex::new(5, 7, 9);
+        a -= Hex{q: 4, r:5, s:6};
+        assert_eq!(a, Hex{q:1,r:2,s:3});
+    }
+
+    #[test]
+    fn div() {
+        assert_eq!(Hex{q:20,r:35,s:54} / Hex{q:4,r:5,s:6}, Hex{q:5,r:7,s:9});
+    }
+
+    #[test]
+    fn div_assign() {
+        let mut a = Hex::new(20, 35, 54);
+        a /= Hex{q: 4, r:5, s:6};
+        assert_eq!(a, Hex{q:5,r:7,s:9});
+    }
+
+    #[test]
+    fn mul() {
+        assert_eq!(Hex{q:5,r:7,s:9} * Hex{q:4,r:5,s:6}, Hex{q:20,r:35,s:54});
+    }
+
+    #[test]
+    fn mul_assign() {
+        let mut a = Hex::new(5, 7, 9);
+        a *= Hex{q: 4, r:5, s:6};
+        assert_eq!(a, Hex{q:20,r:35,s:54});
+    }
+
+    #[test]
+    fn distance() {
+        let a = Hex::new(5, 7, 9);
+        assert_eq!(a.distance(Hex{q: 4, r:5, s: 6}), 3);
     }
 }
